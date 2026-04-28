6e35e34027f4155b277a7e33f8ce13e876e710f4
calyptr4 <alessandrabaires@gmail.com>
2026-04-27 19:19:14 -0700

    a67

--- main.hpp
+++ main.hpp
@@ -5,15 +5,18 @@ int hanoi(int n, char from, char to, char aux);
 
 int hanoi(int n, char from, char to, char aux)
 {
-    static int count = 0;
-    count ++;
     if (n == 1) {
-        cout << "Move disk " << n << " from " << from << " to " << to << endl;
-        return 0;
+        return 1;
     }
 
-    hanoi(n-1, from, aux, to);
+    int count = 0;
+
+    count += hanoi(n-1, from, aux, to);
     cout << "Move disk " << n << " from " << from << " to " << to << endl;
-    hanoi(n-1, aux, to, from);
+    
+    count ++;
+
+    count += hanoi(n-1, aux, to, from);
+    
     return count;
 }
